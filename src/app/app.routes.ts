6ff9904a22fec7f86c8fc83dcffd22bc5eedb882
Dyluan <dylan.devoir@gmail.com>
2025-04-18 13:17:34 +0200

    added the header + first part of homeComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { MainPageComponent } from './pages/main-page/main-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: MainPageComponent
+    }
+];
---
+++ src/app/pages/main-page/main-page.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import { HomeComponent } from '../../components/home/home.component';
+
+@Component({
+  selector: 'app-main-page',
+  imports: [
+    HeaderComponent,
+    HomeComponent,
+  ],
+  templateUrl: './main-page.component.html',
+  styleUrl: './main-page.component.css'
+})
+export class MainPageComponent {
+
+}
