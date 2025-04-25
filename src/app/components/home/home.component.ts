efca41e59f1f74e889f9af2e33f0c045ba66c020
Dyluan <dylan.devoir@gmail.com>
2025-04-25 21:06:32 +0200

    added a typewriter library

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import TypeIt from 'typeit';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,22 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
-
+export class HomeComponent implements AfterViewInit{
+  ngAfterViewInit(): void {
+      new TypeIt('#animated-text', {
+        speed: 100,
+        loop: true,
+        breakLines: false,
+      })
+      .type('front-end developer')
+      .pause(1500)
+      .delete(20)
+      .type('full-stack developer')
+      .pause(1500)
+      .delete(20)
+      .type('creative developer')
+      .pause(1500)
+      .delete(20)
+      .go();
+  }
 }
