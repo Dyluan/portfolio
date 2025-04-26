bff09de80cd28a8855038661acbd68293d0c039f
Dyluan <dylan.devoir@gmail.com>
2025-04-26 17:06:51 +0200

    added some styling on the dropdown menu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   imports: [
     TranslateModule,
+    CommonModule,
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+  isDropDownOpen = false;
 
   constructor(private translate: TranslateService) {
     translate.setDefaultLang('en');
@@ -18,6 +22,21 @@ export class HeaderComponent {
 
   switchLang(lang: string) {
     this.translate.use(lang);
+    this.isDropDownOpen = false; // Close the dropdown after selecting a language
   }
 
+  toggleDropDown() {
+    this.isDropDownOpen = !this.isDropDownOpen;
+  }
+  getCurrentLang() {
+    return this.translate.currentLang || 'en';
+  }
+
+  @HostListener('document:click', ['$event'])
+    onDocumentClick(event: MouseEvent) {
+        const dropdown = document.querySelector('.languageSelector');
+        if (!dropdown?.contains(event.target as Node)) {
+            this.isDropDownOpen = false;
+        }
+    }
 }
