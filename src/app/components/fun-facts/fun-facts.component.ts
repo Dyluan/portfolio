0d8e0c2222afbfe36c97560f374d717f5e876a9d
Dyluan <dylan.devoir@gmail.com>
2025-05-01 12:16:24 +0200

    modified the fun facts to be translated

--- src/app/components/fun-facts/fun-facts.component.ts
+++ src/app/components/fun-facts/fun-facts.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-fun-facts',
-  imports: [],
+  imports: [
+    TranslateModule,
+  ],
   templateUrl: './fun-facts.component.html',
   styleUrl: './fun-facts.component.css'
 })
 export class FunFactsComponent {
+  facts: string[] = [];
+
+  constructor(private translate: TranslateService) {
+    this.translate.get('FUN_FACTS.FACTS').subscribe((facts: string[]) => {
+      this.facts = facts;
+    });
+
+    this.translate.onLangChange.subscribe(() => {
+      this.translate.get('FUN_FACTS.FACTS').subscribe((facts: string[]) => {
+        this.facts = facts;
+      });
+    });
+  }
 
 }
