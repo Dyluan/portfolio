e4abfdae950f02dead341a3556bf67eed315b28b
Dyluan <dylan.devoir@gmail.com>
2025-05-10 14:49:48 +0200

    website in prod

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -18,20 +18,27 @@ export class HomeComponent implements AfterViewInit, OnDestroy{
   private languageSubscription: Subscription;
 
   constructor(private translate: TranslateService) {
-    //s'abonner aux changements de langue
     this.languageSubscription = this.translate.onLangChange.subscribe(() => {
       if (this.typeItInstance) {
         this.typeItInstance.destroy();
         setTimeout(() => {
           this.initTypeIt();
-        }, 100)
+          console.log('constructor called');
+        }, 1000)
       }
     })
   }
 
   ngAfterViewInit(): void {
+    //added a timeout to ensure the DOM is ready before initializing TypeIt
+    // This is a workaround, ideally you should ensure the DOM is ready before this point
+    setTimeout(() => {
       this.initTypeIt();
+      console.log('afterviewinit called');
+    }, 1000);
+      
   }
+
   ngOnDestroy(): void {
     if (this.languageSubscription) {
       this.languageSubscription.unsubscribe();
@@ -51,20 +58,22 @@ export class HomeComponent implements AfterViewInit, OnDestroy{
     this.typeItInstance = new TypeIt('#animated-text', {
       speed: 100,
       loop: true,
-      breakLines: true,
+      breakLines: false,
       startDelete: true,
       waitUntilVisible: true,
       nextStringDelay: 1000,
+
     })
+    .empty()
     .type(titles[0])
     .pause(1500)
-    .delete(titles[0].length)
+    .delete()
     .type(titles[1])
     .pause(1500)
-    .delete(titles[1].length)
+    .delete()
     .type(titles[2])
     .pause(1500)
-    .delete(titles[2].length)
+    .delete()
     .go();
   }
 
